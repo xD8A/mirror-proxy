151c3ac811f3b9163f83f6e169a8ffa98a87a905
Dmitry Antipin <antipin.dmitry@gmail.com>
2021-01-03 13:08:35 +0400

    Implement proxy server

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use std::process::exit;
 use std::format;
-use std::convert::TryFrom;
+use std::convert::{TryFrom,Infallible};
 use std::io::Write;
-use log::{info, warn, error};
+use std::net::{ToSocketAddrs, SocketAddr};
+use log::{info, warn, error, debug};
+use futures_util::future::try_join;
 use clap::{App, Arg};
+use http;
+use tokio::net::TcpStream;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::upgrade::Upgraded;
+use hyper::{Body, Client, Method, Request, Response, Server};
+use hyper::server::conn::AddrStream;
+
+
+pub type HttpClient = Client<hyper::client::HttpConnector>;
 
 
 #[tokio::main]
@@ -56,7 +67,7 @@ async fn main() {
         Some(v) => match v.parse::<u16>() {
             Ok(v) => v,
             Err(_) => {
-                warn!("args: invalid set port (must be a number in the range 0..65535, got {:?}) \
+                warn!("invalid set port in args (must be a number in the range 0..65535, got {:?}) \
                        will be changed to default value ({})", v, DEFAULT_PORT);
                 DEFAULT_PORT
             }
@@ -104,8 +115,8 @@ async fn main() {
                     _ => None
                 };
                 if p.is_none() {
-                    warn!("args: invalid set port (must be a number in the range 0..65535, got {:?}) \
-                       will be changed to default value ({})", v, port);
+                    warn!("invalid set port in config (must be a number in the range 0..65535, got {:?}) \
+                           will be changed to default value ({})", v, port);
                 }
                 p
             }
@@ -116,5 +127,137 @@ async fn main() {
         }
     }
 
-    info!("server listening at {}:{}", ip, port);
+    let addr = match to_addr(format!("{}:{}", ip, port)) {
+        Some(v) => v,
+        None => {
+            error!("can not resolve server address {}:{}", ip, port);
+            exit(78);
+        }
+    };
+    let client = HttpClient::new();
+
+    let make_service = make_service_fn(move |conn: &AddrStream| {
+        let client = client.clone();
+        let peer = conn.remote_addr();
+        async move { Ok::<_, Infallible>(service_fn(move |req| proxy(client.clone(), req, peer))) }
+    });
+
+    let server = Server::bind(&addr).serve(make_service);
+
+    info!("server listening at {}", addr);
+
+    if let Err(e) = server.await {
+        error!("server crashed; err = {:?}", e);
+    }
+}
+
+fn to_addr(host: String) -> Option<SocketAddr> {
+
+    let mut addrs_iter = match host.to_socket_addrs() {
+        Ok(v) => v,
+        Err(_) => {
+            return None;
+        }
+    };
+
+    match addrs_iter.next() {
+        Some(v) => Some(v),
+        None => None
+    }
+
+}
+
+async fn proxy(client: HttpClient, req: Request<Body>, peer: SocketAddr) -> Result<Response<Body>, hyper::Error> {
+    info!("client {:?}: connected", peer);
+    debug!("client {:?}: request = {:?}", peer, req);
+
+    if Method::CONNECT == req.method() {
+        // Creates a tunnel between the client and the remote server
+        //
+        //            Client                     Forward Proxy                    Server
+        //               CONNECT request ------------->
+        //                                             CONNECT request ------------->
+        //                                             <-------- CONNECT response: OK
+        //               <-------- CONNECT response: OK
+        //               <--------------------- SSL negotiations ------------------->
+        //               ------------------------ HTTP request --------------------->
+        //               <----------------------- HTTP response -------------------->
+        //
+        // Received an HTTP request like:
+        // ```
+        // CONNECT www.domain.com:443 HTTP/1.1
+        // Host: www.domain.com:443
+        // Proxy-Connection: Keep-Alive
+        // ```
+        //
+        // When HTTP method is CONNECT we should return an empty body
+        // then we can eventually upgrade the connection and talk a new protocol.
+        //
+        // Note: only after client received an empty body with STATUS_OK can the
+        // connection be upgraded, so we can't return a response inside
+        // `on_upgrade` future.
+        //
+        let uri = req.uri();
+        let addr = match uri.authority() {
+            Some(v) => {
+                let host = v.to_string();
+                to_addr(host)
+            },
+            None => None
+        };
+        if addr.is_some() {
+            let addr = addr.unwrap();
+            error!("client {:?}: upstream remote uri {:?}", peer, uri);
+            tokio::task::spawn(async move {
+                match hyper::upgrade::on(req).await {
+                    Ok(upgraded) => {
+                        if let Err(e) = tunnel(upgraded, addr, peer).await {
+                            error!("client {:?}: server io error; err = {:?}", peer, e);
+                        };
+                        info!("client {:?}: connection closed", peer);
+                    }
+                    Err(e) => error!("client {:?}: upgrade error; err = {:?}", peer, e),
+                }
+            });
+            Ok(Response::new(Body::empty()))
+        } else {
+            error!("client {:?}: cannot resolve remote uri {:?}", peer, uri);
+            let mut resp = Response::new(Body::from(format!("cannot resolve remote uri {:?}", uri)));
+            *resp.status_mut() = http::StatusCode::BAD_REQUEST;
+            Ok(resp)
+        }
+    } else {
+        client.request(req).await.and_then(|resp| {
+            info!("client {:?}: connection closed", peer);
+            Ok(resp)
+        })
+    }
+}
+
+
+async fn tunnel(upgraded: Upgraded, addr: SocketAddr, peer: SocketAddr) -> std::io::Result<()> {
+    // Connect to remote server
+    let mut server = TcpStream::connect(addr).await?;
+
+    // Proxying data
+    let amounts = {
+        let (mut server_rd, mut server_wr) = server.split();
+        let (mut client_rd, mut client_wr) = tokio::io::split(upgraded);
+
+        let client_to_server = tokio::io::copy(&mut client_rd, &mut server_wr);
+        let server_to_client = tokio::io::copy(&mut server_rd, &mut client_wr);
+
+        try_join(client_to_server, server_to_client).await
+    };
+
+    // Print message when done
+    match amounts {
+        Ok((from_client, from_server)) => {
+            debug!("client {:?}: {} - wrote {} bytes and received {} bytes", peer, addr, from_client, from_server);
+        }
+        Err(e) => {
+            error!("client {:?}: tunnel error err = {:?}", peer, e);
+        }
+    };
+    Ok(())
 }
