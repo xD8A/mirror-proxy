41026bb4c44916393d902d58849868be9bde80a7
Dmitry Antipin <antipin.dmitry@gmail.com>
2021-01-03 09:47:08 +0400

    Setup logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use std::io::Write;
+use log::info;
+
 
 #[tokio::main]
 async fn main() {
+    // setup logging
+    env_logger::Builder::new()
+        .format(|in_buf, record| {
+            writeln!(in_buf,
+                     "{} [{}] - {}",
+                     chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                     record.level(),
+                     record.args()
+            )
+        })
+        .filter(None, log::LevelFilter::Debug)
+        .init();
+
+    info!("Server listening at 127.0.0.1:8080");
 }
